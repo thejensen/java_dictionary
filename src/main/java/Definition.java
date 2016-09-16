c0ca905b9f7beaf7183893402910d3c847ec12d3
Sara Jensen <sara.jensen@idealist.org>
2016-09-16 09:17:15 -0700

    add basic setup to Word.java, Definition.java, the Test files, App.java, and templates.

---
+++ src/main/java/Definition.java
@@ -0,0 +1,12 @@
+public class Definition {
+  private String mWhatevs;
+
+}
+
+public Definition(String whatevs) {
+  mWhatevs = whatevs;
+}
+
+public String getWhatevs() {
+  return mWhatevs;
+}
--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,1 +1,12 @@
+public class Word {
+  private String mYaaas;
 
+}
+
+public Definition(String yaaas) {
+  mYaaas = yaaas;
+}
+
+public String getYaaas() {
+  return mYaaas;
+}
